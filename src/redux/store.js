99a0aba49740f0d58f8cf0da813259080d825db4
Anna Kravets <annakravets1906@gmail.com>
2023-06-12 23:12:09 +0300

    Added whitelist

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,15 +10,10 @@ import { rootReducer } from './reducer';
 const persistConfig = {
   key: 'contacts',
   storage,
+  whitelist: ['contacts'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// export default () => {
-//   let store = createStore(persistedReducer);
-//   let persistor = persistStore(store);
-//   return { store, persistor };
-// };
 
 export const store = configureStore({
   reducer: persistedReducer,
